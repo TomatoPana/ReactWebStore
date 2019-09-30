8437c31133a3ac9f979ef31a533b208694ae5e8b
Moises David <mdlb.lobo@gmail.com>
2019-09-30 11:54:19 -0500

    Añadido control de entrada de correo

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import { Grid } from '@material-ui/core';
 
 
 
 export default function LoginComponent(): JSX.Element {
-  return <></>
+  return (
+    <Grid container justify="center" alignItems="center">
+      <Grid item xs={12} md={8} lg={6}>
+        
+      </Grid>
+    </Grid>
+  )
 }

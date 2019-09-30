48bdc6b5e2c0164fb7cffdd8f2cd8c040c54cb35
Moises David <mdlb.lobo@gmail.com>
2019-09-30 11:48:19 -0500

    Reorganización de la estructura de archivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import TextFields from './components/TextFields';
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <TextFields />
-    </div>
+    <TextFields />
   );
 }
 

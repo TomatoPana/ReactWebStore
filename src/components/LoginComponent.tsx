d136c3a29b72fdd2fcd1557b31247e92a842cf63
Moises David Bobadilla Lozano <mdlb.lobo@gmail.com>
2019-09-30 20:55:24 -0500

    Terminado LoginComponent

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,53 +1,130 @@
 import React from "react";
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import { Grid } from '@material-ui/core';
-import EmailInputController from './InputControllers/EmailInputController';
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: '100%',
-    }
-  }),
-);
-
-export default function LoginComponent(): JSX.Element {
-  //const Component = EmailInputController(TextField, {label: "name"});
-  const classes = useStyles();
+function Copyright() {
   return (
-    <Grid container direction="column" justify="center" alignItems="center">
-      <Grid item sm={12} md={8} lg={4}>
-        <TextField
-          id="email"
-          label="Email"
-          type="email"
-          name="email"
-          placeholder="someone@example.com"
-          className={classes.textField}
-          helperText="Enter a valid Email"
-          autoComplete="email"
-          margin="normal"
-          variant="outlined"
-          fullWidth
-        />
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  paper: {
+    marginTop: '50%',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  },
+  container: {
+    width:'100%',
+    heigth:'100%'
+  }
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container} component="main" maxWidth="xs">
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Grid item>
+          <CssBaseline />
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+          <Box mt={8}>
+            <Copyright />
+          </Box>
+        </Grid>
       </Grid>
-      <Grid item sm={12} md={8} lg={4}>
-        <TextField
-          id="password"
-          label="Password"
-          type="password"
-          name="password"
-          className={classes.textField}
-          helperText="Enter a password"
-          autoComplete="current-password"
-          margin="normal"
-          variant="outlined"
-          fullWidth
-        />
-      </Grid>
-    </Grid>
-  )
+    </Container>
+  );
 }

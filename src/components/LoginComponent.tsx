31775272deef05011881a60382fd794be0f745d8
Moises David <mdlb.lobo@gmail.com>
2019-09-30 20:16:46 -0500

    Añadido campos de texto al login

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -4,12 +4,49 @@ import TextField from '@material-ui/core/TextField';
 import { Grid } from '@material-ui/core';
 import EmailInputController from './InputControllers/EmailInputController';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: '100%',
+    }
+  }),
+);
+
 export default function LoginComponent(): JSX.Element {
-  const Component = EmailInputController(TextField, {label: "name"});
+  //const Component = EmailInputController(TextField, {label: "name"});
+  const classes = useStyles();
   return (
-    <Grid container justify="center" alignItems="center">
-      <Grid item xs={12} md={8} lg={6}>
-        <Component/>
+    <Grid container direction="column" justify="center" alignItems="center">
+      <Grid item sm={12} md={8} lg={4}>
+        <TextField
+          id="email"
+          label="Email"
+          type="email"
+          name="email"
+          placeholder="someone@example.com"
+          className={classes.textField}
+          helperText="Enter a valid Email"
+          autoComplete="email"
+          margin="normal"
+          variant="outlined"
+          fullWidth
+        />
+      </Grid>
+      <Grid item sm={12} md={8} lg={4}>
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          name="password"
+          className={classes.textField}
+          helperText="Enter a password"
+          autoComplete="current-password"
+          margin="normal"
+          variant="outlined"
+          fullWidth
+        />
       </Grid>
     </Grid>
   )
